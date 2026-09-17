9aa1e366187d9e4d9014539f3b2998f525f9a0d7
GCurie <curieuniv@gmail.com>
2026-09-16 18:41:08 -0700

    basic template with functions

---
+++ sketch.js
@@ -0,0 +1,12 @@
+console.log("I believe I can fly");
+
+function setup() {
+  createCanvas(800, 600);
+}
+
+function draw() {
+  background(20);
+  noStroke();
+  fill(255, 120, 60);
+  circle(mouseX, mouseY, 50);
+}
